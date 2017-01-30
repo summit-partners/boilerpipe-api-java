fa528bd821faeb16517ac3ac8a0145a9735ebd9b
Tadas Subonis <tadas.subonis@gmail.com>
2017-01-30 16:44:20 +0100

    Optimize API for Boilerpipe. Ignore small images.

--- src/main/java/org/boilerpipe/web/BoilerpipeService.java
+++ src/main/java/org/boilerpipe/web/BoilerpipeService.java
@@ -36,32 +36,13 @@ public class BoilerpipeService {
     public Object getContent(ExtractionRequest request) {
         try {
             String html = request.html;
-            String url = request.url;
 
             if (request.extractHtml) {
-                HTMLHighlighter hh = HTMLHighlighter.newExtractingInstance();
-
-                InputSource is1 = new InputSource(new StringReader(html));
-                TextDocument doc = new BoilerpipeSAXInput(is1).getTextDocument();
-
-                articleExtractor.process(doc);
-
-                InputSource is = new InputSource(new StringReader(html));
-                return hh.process(doc, is);
+                return extractHtml(html);
             }
 
             if (request.extractImages) {
-                TextDocument doc = new BoilerpipeSAXInput(new InputSource(new StringReader(html))).getTextDocument();
-                articleExtractor.process(doc);
-
-                InputSource is = new InputSource(new StringReader(html));
-
-                List<Image> process = imageExtractor.process(doc, is);
-
-                return process.stream()
-                        .map(Image::getSrc)
-                        .collect(Collectors.toList());
-
+                return extractImages(html);
             }
 
             throw new RuntimeException("Not Supported operation");
@@ -69,5 +50,85 @@ public class BoilerpipeService {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @POST
+    @Path("/extractHtml")
+    @Consumes("text/html")
+    @Produces("application/json")
+    public String extractHtml(String html) {
+        try {
+            HTMLHighlighter hh = HTMLHighlighter.newExtractingInstance();
+
+            InputSource is1 = new InputSource(new StringReader(html));
+            TextDocument doc = new BoilerpipeSAXInput(is1).getTextDocument();
+
+            articleExtractor.process(doc);
+
+            InputSource is = new InputSource(new StringReader(html));
+            return hh.process(doc, is);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @POST
+    @Path("/extractImages")
+    @Consumes("text/html")
+    @Produces("application/json")
+    public List<String> extractImages(String html) {
+        try {
+            TextDocument doc = new BoilerpipeSAXInput(new InputSource(new StringReader(html))).getTextDocument();
+            articleExtractor.process(doc);
+
+            InputSource is = new InputSource(new StringReader(html));
+
+            List<Image> process = imageExtractor.process(doc, is);
+
+            return process.stream()
+                    .filter(image -> {
+                        String width = toEmpty(image.getWidth());
+                        String height = toEmpty(image.getHeight());
+
+                        if (width.isEmpty() && height.isEmpty()) {
+                            return true;
+                        }
+
+                        if (width.contains("%")) {
+                            return true;
+                        }
+
+                        if (getInteger(width) < 100) {
+                            return false;
+                        }
+
+                        if (getInteger(height) < 100) {
+                            return false;
+                        }
+
+                        return true;
+                    })
+                    .map(Image::getSrc)
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private int getInteger(String width) {
+        try {
+            return Integer.parseInt(width);
+        } catch (NumberFormatException e) {
+            logger.warn("Could not Parse integer %s", width);
+            return 0;
+        }
+    }
+
+    private String toEmpty(String string) {
+        if (string == null) {
+            return "";
+        }
+
+        return string;
     }
 }
