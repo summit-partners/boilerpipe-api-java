cb489f067a853ffa2015968e3411e0af65135aeb
Alex Vollmer <avollmer@summitpartners.com>
2019-07-19 15:22:00 -0700

    Return highlighted content
    Reworked /extractText endpoint so that now it returns a JSON payload
    consisting of both the extracted plain-text as well as the highlighted
    HTML. The latter attribute is used in the webcrawl to limit the HTML from
    which links are extracted.

--- src/main/java/org/boilerpipe/web/ExtractionResponse.java
+++ src/main/java/org/boilerpipe/web/ExtractionResponse.java
@@ -1,5 +1,12 @@
 package org.boilerpipe.web;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class ExtractionResponse {
 
+  @JsonProperty("highlighted")
+  String highlighted;
+
+  @JsonProperty("plain")
+  String plain;
 }
